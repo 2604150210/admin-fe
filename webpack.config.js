af6590b2b2221b725d902ce3b6133d1330053439
Cathy <517486222@qq.com>
2019-10-11 17:49:15 +0800

    online adapter

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,13 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+let WEBPACK_ENV = process.env.WEBPACK_ENV || 'dev';
+console.log(WEBPACK_ENV);
 module.exports = {
   entry: './src/app.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/',
+    publicPath: WEBPACK_ENV === 'dev' ? '/dist/' : '//s.jianliwu.com/admin-v2-f2/dist/',
     filename: 'js/app.js',
   },
   resolve: {
