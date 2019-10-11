6bc4a4ef0e73dee6a9935c68d9a0257b9e375e6a
Cathy <517486222@qq.com>
2019-10-11 13:05:05 +0800

    order订单列表、详情

---
+++ src/service/order-service.jsx
@@ -0,0 +1,43 @@
+import MUtil from 'util/mm.jsx';
+
+const _mm = new MUtil();
+class Order{
+	// 获取商品列表
+	getOrderList(listParam){
+		let url = '', data = {};
+		if(listParam.listType === 'list'){
+			url = '/manage/order/list.do';
+			data.pageNum = listParam.pageNum;
+		}else if(listParam.listType === 'search'){
+			url = '/manage/order/search.do';
+			data.pageNum = listParam.pageNum;
+			data.orderNo = listParam.orderNo;
+		}
+		return _mm.request({
+			type: 'post',
+			url: url,
+			data: data
+		});
+	}
+	// 获取订单详情
+	getOrderDetail(orderNumber){
+		return _mm.request({
+			type: 'post',
+			url: '/manage/order/detail.do',
+			data: {
+				orderNo: orderNumber
+			}
+		});
+	}
+	// 订单发货
+	sendGoods(orderNumber){
+		return _mm.request({
+			type: 'post',
+			url: '/manage/order/send_goods.do',
+			data: {
+				orderNo: orderNumber
+			}
+		});
+	}
+}
+export default Order;
